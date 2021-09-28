72b7e1796e535e67f0ca4bcab79817a67649d071
David Egloh <davidegloh.oclock@gmail.com>
2021-09-28 11:07:22 +0200

    modifier only owner

--- Index.sol
+++ Index.sol
@@ -7,7 +7,7 @@ contract Wallet {
     address[] public owners; 
     uint limit;
 
-// Struct d'un transfert 
+// Struct d'une transaction 
 
     struct Transfer{
         uint id;
@@ -16,6 +16,20 @@ contract Wallet {
         uint approvals;
         bool hasBeenSent;
          
+    }
+
+// Tableau des demandes de transfert
+Transfer[] transferRequests;
+
+// Mapping approvals
+mapping(address => mapping(uint => bool)) approvals;
+mapping(address => bool) public isOwner;
+
+// Modifiers
+
+//Should only allow people in the owners list to continue the execution.
+    modifier onlyOwners(){
+        require (isOwner[msg.sender] "not owner");
     }
 
 
