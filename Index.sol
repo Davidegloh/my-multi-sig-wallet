f9f4870706a871ab789a5784f6b7638f9e9bcef1
David Egloh <davidegloh.oclock@gmail.com>
2021-09-28 16:04:39 +0200

    function setApproval + event setApprovals

--- Index.sol
+++ Index.sol
@@ -3,6 +3,9 @@ pragma abicoder v2;
 
 
 contract Wallet {
+    //Events
+    event setApprovals(address indexed owner, uint txIndex);
+
     // Tableau des owners du contrat 
     address[] public owners; 
     // state variable of the limit of approvals
@@ -30,7 +33,8 @@ mapping(address => bool) public isOwner;
 
 //Should only allow people in the owners list to continue the execution.
     modifier onlyOwners(){
-        require (isOwner[msg.sender] "not owner");
+        require (isOwner[msg.sender], "not owner");
+        _;
     }
 
     //Should initialize the owners list and the limit 
@@ -45,4 +49,17 @@ mapping(address => bool) public isOwner;
     //Empty function
     function deposit() public payable {}
 
+    //Set your approval for one of the transfer requests.
+    function setApproval(uint _id) public onlyOwner {
+        Transfer storage transfer =  transferRequests[_id]; // it allows to update the transaction struct and to assign the value passed input to the storage variable transaction
+        transfer.approvals += 1; 
+        approvals[msg.sender][_id] = true; 
+        emit setApprovals(msg.sender, _id);
+    }
+
 }
+/*
+        Transaction storage transaction = transactions[_txIndex];
+        transaction.numConfirmations += 1;
+        isConfirmed[_txIndex][msg.sender] = true;
+        /*
