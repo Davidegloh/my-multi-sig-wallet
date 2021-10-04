fa0fbc173ca9970914c44d1212955399272184ac
David Egloh <davidegloh.oclock@gmail.com>
2021-10-04 11:22:43 +0200

    modifier onlyOwner

--- Index.sol
+++ Index.sol
@@ -30,12 +30,18 @@ mapping(address => mapping(uint => bool)) approvals;
 mapping(address => bool) public isOwner;
 
 // Modifiers
-
 //Should only allow people in the owners list to continue the execution.
     modifier onlyOwners(){
-        require (isOwner[msg.sender], "not owner");
-        _;
-    }
+        bool owner = false;
+        for(i=0; i<owners.length; i++){
+            if (owners[i] == msg.sender){
+                owner = true;
+            }
+            require (owner = true);
+            _;
+        }
+        
+
 
     //Should initialize the owners list and the limit 
     constructor(address[] memory _owners, uint _limit) {
@@ -46,8 +52,15 @@ mapping(address => bool) public isOwner;
 
     }
 
-    //Empty function
+    //Empty deposit function
     function deposit() public payable {}
+
+    //Create an instance of the Transfer struct and add it to the transferRequests array
+    function createTransfer(uint _amount, address payable _receiver) public onlyOwners { // I create the function "createTransfer" with 2 inputs (amount and receiver)
+        transferRequests.push(// I add the Transfer instance to the transferRequests array
+        Transfer(_amount, _receiver, 0, false, transferRequests.length) //I create an instance of Transfer with the proper properties of a Transaction.
+        );
+    }
 
     //Set your approval for one of the transfer requests.
     function setApproval(uint _id) public onlyOwner {
@@ -55,8 +68,9 @@ mapping(address => bool) public isOwner;
         transfer.approvals += 1; 
         approvals[msg.sender][_id] = true; 
         emit setApprovals(msg.sender, _id);
+        require();
     }
-   //Need to update the Transfer object
+
     //Need to update the mapping to record the approval for the msg.sender.
     //When the amount of approvals for a transfer has reached the limit, this function should send the transfer to the recipient.
     //An owner should not be able to vote twice.
