53b965b94d2880e3d341d597954bcd7add9cb463
David Egloh <davidegloh.oclock@gmail.com>
2021-10-06 10:29:01 +0200

    function SetApproval debug

--- Index.sol
+++ Index.sol
@@ -6,12 +6,12 @@ contract Wallet {
     //Events
     event setApprovals(address indexed owner, uint txIndex);
 
-    // Tableau des owners du contrat 
+    // Array of owners of the contract
     address[] public owners; 
     // state variable of the limit of approvals
     uint limit;
 
-// Struct d'une transaction 
+// Struct of a transaction 
 
     struct Transfer{
         uint id;
@@ -22,11 +22,12 @@ contract Wallet {
          
     }
 
-// Tableau des demandes de transfert
+// Array of transfer requests
 Transfer[] transferRequests;
 
 // Mapping approvals
 mapping(address => mapping(uint => bool)) approvals;
+// Mapping isOwner
 mapping(address => bool) public isOwner;
 
 // Modifiers
@@ -65,8 +66,6 @@ mapping(address => bool) public isOwner;
     
     //Need to update the mapping to record the approval for the msg.sender.
     //When the amount of approvals for a transfer has reached the limit, this function should send the transfer to the recipient.
-
-    
     //Set your approval for one of the transfer requests
     function setApproval(uint _id) public onlyOwner {
         Transfer storage transfer =  transferRequests[_id]; // it allows to update the transaction struct and to assign the value passed input to the storage variable transaction
@@ -78,12 +77,12 @@ mapping(address => bool) public isOwner;
 
         if(transferRequests[_id].approvals >=limit){
             transferRequests[_id].hasBeenSent = true;
-            transferRequests[_id].receiver.tranfer(transferRequests[_id].amount);
+            transferRequests[_id].receiver.transfer(transferRequests[_id].amount);
         }
    
     }
 
-//Should reutn all transfer requests
+//Should reuturn all transfer requests
 function getTransfertRequests() public view returns(Transfer[]memory){
     return transferRequests;
 }
