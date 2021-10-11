aa4a905614048f3330617d274d3a5adb3af2e465
David Egloh <davidegloh.oclock@gmail.com>
2021-10-11 12:22:55 +0200

    emit added

--- Index.sol
+++ Index.sol
@@ -19,7 +19,7 @@ contract Wallet {
     }
 
 //Events
-    event setApprovals(address indexed owner, uint txIndex);
+    event TransferRequestCreated(uint _id, uint amount, address initiator, address _receiver);
     event ApprovalReceived(uint _id, uint approvals, address approver);
     event TransferApproved(uint _id);
 
@@ -69,6 +69,7 @@ mapping(address => uint) balance;
 
     //Create an instance of the Transfer struct and add it to the transferRequests array. I use it when an owner wants to initiate a transfer
     function createTransfer(uint _amount, address payable _receiver) public onlyOwners { // I create the function "createTransfer" with 2 inputs (amount and receiver)
+        emit TransferRequestCreated(transferRequests.length, _amount, msg.sender, _receiver);
         transferRequests.push(// I add the Transfer instance to the transferRequests array
         Transfer(0, _amount, _receiver,transferRequests.length,false) //I create an instance of Transfer with the proper properties of a Transaction.
         );
@@ -87,9 +88,12 @@ mapping(address => uint) balance;
         approvals[msg.sender][_id] = true;
         transferRequests[_id].approvals++; ////Need to update the mapping to record the approval for the msg.sender.
 
+        emit ApprovalReceived(_id,transferRequests[_id].approvals, msg.sender);
+
         if(transferRequests[_id].approvals >=limit){ // //When the amount of approvals for a transfer has reached the limit, this function should send the transfer to the recipient.
             transferRequests[_id].hasBeenSent = true;
             transferRequests[_id].receiver.transfer(transferRequests[_id].amount);
+            emitTransferApproved(_id);
         }
    
     }
